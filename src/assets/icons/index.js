1c558cda9cb4d8d039ece2c0cf59f92d4aafd7ae
Pro-100-kent <pro100kent@gmail.com>
2019-12-19 11:14:02 +0300

    Доделал экран пустого списка покупок.

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -2,6 +2,7 @@ const icons = {
   cross: require('./cross2.png'),
   cart: require('./cart2.png'),
   checkmark: require('./checkmark.png'),
+  cartWithArrow: require('./cart5.png'),
 };
 
 export {icons};
--- src/components/EmptyShoppingListScreen.js
+++ src/components/EmptyShoppingListScreen.js
@@ -6,7 +6,15 @@ export default class EmptyShoppingListScreen extends Component {
   render() {
     return (
       <View style={styles.mainContainer}>
-        <Text>EmptyShoppingListScreen</Text>
+        <View style={styles.emptyIconContainer}>
+          <Image style={styles.emptyIcon} source={icons.cartWithArrow} />
+        </View>
+        <View style={styles.emptyTextContainer}>
+          <Text style={styles.emptyHeaderText}>Список пуст</Text>
+          <Text style={styles.emptyExplanationText}>
+            Добавьте сюда то, что вы планируете купить
+          </Text>
+        </View>
       </View>
     );
   }
@@ -17,6 +25,28 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'flex-start',
     alignItems: 'center',
-    backgroundColor: 'green',
+  },
+  emptyIconContainer: {
+    flex: 1,
+    alignSelf: 'stretch',
+  },
+  emptyIcon: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    resizeMode: 'contain',
+  },
+  emptyTextContainer: {
+    margin: 23,
+    alignItems: 'center',
+  },
+  emptyHeaderText: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  emptyExplanationText: {
+    marginTop: 7,
+    fontSize: 19,
+    textAlign: 'center',
   },
 });
