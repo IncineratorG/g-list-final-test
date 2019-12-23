9cdfe763aa72526338f126e8e4e9c24ca35d5f68
Pro-100-kent <pro100kent@gmail.com>
2019-12-23 17:23:24 +0300

    Создал заготовку экрана создания\редакстирования элемента списка покупок.

--- src/components/AddButton.js
+++ src/components/AddButton.js
@@ -4,22 +4,22 @@
 * */
 
 import React, {Component} from 'react';
-          import {View, StyleSheet, Image, TouchableHighlight} from 'react-native';
-          import {icons} from '../assets/icons';
+import {View, StyleSheet, Image, TouchableHighlight} from 'react-native';
+import {icons} from '../assets/icons';
 
-          export default class AddButton extends Component {
-          render() {
-          return (
-          <TouchableHighlight
-          style={styles.touchable}
-          onPress={() => {
-              if (this.props.onClick !== undefined) {
-                  this.props.onClick();
-              }
-          }}>
-          <View style={styles.mainContainer}>
+export default class AddButton extends Component {
+  render() {
+    return (
+      <TouchableHighlight
+        style={styles.touchable}
+        onPress={() => {
+          if (this.props.onClick !== undefined) {
+            this.props.onClick();
+          }
+        }}>
+        <View style={styles.mainContainer}>
           <Image style={styles.crossIcon} source={icons.cross} />
-          </View>
+        </View>
       </TouchableHighlight>
     );
   }
